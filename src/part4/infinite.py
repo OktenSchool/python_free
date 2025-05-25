1945fa32fe33dfed9cb3d887a01c22fd60d8ddbf
GrayHead <ss.zhuravlov@gmail.com>
2025-05-25 14:35:24 +0300

    commit push

---
+++ src/part4/infinite.py
@@ -0,0 +1,13 @@
+counter = 8
+
+# while True:
+#     counter += 1
+#     print(counter)
+#     if counter == 10_000:
+#         break
+
+while True:
+    counter += 1
+    print(counter)
+    if counter % 7 == 0 :
+        break
